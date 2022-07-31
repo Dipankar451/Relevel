49f73ef897095b2e433af9826320825867811f48
Dipankar Ghosh <dipankargh786@gmail.com>
2022-07-31 21:48:59 +0530

    first commit

---
+++ Assignment_1_aug_1.js
@@ -0,0 +1,8 @@
+
+function astroguru(num_of_children, spouses_name, job_location, job_name){
+    var future = " You will be a " + job_name + " in " + job_location + " married to " + spouses_name + ". " + " You will have " + num_of_children + " kids. ";
+
+    console.log(future);
+}
+
+astroguru(" 2 " , " Mary " , " Hyderabad " , " Software Engineer ");
---
+++ Assignment_1_aug_2.js
@@ -0,0 +1,16 @@
+
+function compareNum(a, b){
+    if (a>b){
+        console.log(" A is greater than B ");
+    }
+    else if(b>a){
+        console.log(" B is greater than A ");
+    }
+    else {
+        console.log(" Both are Same numbers ");
+    }
+}
+
+compareNum(55, 45);
+compareNum(45, 65);
+compareNum(55, 55);
